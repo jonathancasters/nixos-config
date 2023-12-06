20fa82c68df5e3dfedf602a89f9d0342802efb3d
jonathancasters <jonathancasters284@gmail.com>
2023-12-06 11:08:30 +0100

    add completion support + change to native lsp

--- modules/editors/nvim.nix
+++ modules/editors/nvim.nix
@@ -11,6 +11,8 @@ let
     local o = vim.o
     -- vim global variables
     local g = vim.g
+    -- vim api
+    local api = vim.api
 
     -- utility function that helps remapping keys to commands
     function map(mode, keys, command)
@@ -78,22 +80,6 @@ let
     require("nvim-tree").setup()
     -- enable icons in tree
     require("nvim-web-devicons").setup()
-
-
-    -- COC-SETTINGS
-    -- To support docker-compose auto-completion
-    vim.api.nvim_exec([[
-      augroup FileTypeYaml
-        autocmd!
-        autocmd FileType yaml if vim.fn.expand('%:t') == 'docker-compose.yml' | setlocal filetype=yaml.docker-compose | endif
-        autocmd FileType yaml if vim.fn.expand('%:t') == 'compose.yml' | setlocal filetype=yaml.docker-compose | endif
-      augroup END
-    ]], false)
-
-    g.coc_filetype_map = {
-      ['yaml.docker-compose'] = 'dockercompose',
-    }
-
     -- ENABLE PLUGINS
     vim.o.number = true
     
@@ -113,15 +99,167 @@ let
     })
 
     vim.cmd.colorscheme "catppuccin-latte"
+
+    o.tabstop = 4
+    o.shiftwidth = 4
+  '';
+
+  # language servers
+  language-servers = ''
+    local lspconfig = require('lspconfig')
+    local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+    -- C/C++
+    lspconfig.clangd.setup({
+      capabilities = lsp_capabilities,
+    })
+    -- Nix
+    lspconfig.nixd.setup({
+      capabilities = lsp_capabilities,
+    })
+
+    api.nvim_create_autocmd('LspAttach', {
+      desc = 'LSP actions',
+      callback = function()
+        local bufmap = function(mode, lhs, rhs)
+          local opts = {buffer = true}
+          vim.keymap.set(mode, lhs, rhs, opts)
+        end
+
+        -- Displays hover information about the symbol under the cursor
+        bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+
+        -- Jump to the definition
+        bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+
+        -- Jump to declaration
+        bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+
+        -- Lists all the implementations for the symbol under the cursor
+        bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+
+        -- Jumps to the definition of the type symbol
+        bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+
+        -- Lists all the references 
+        bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+
+        -- Displays a function's signature information
+        bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+
+        -- Renames all references to the symbol under the cursor
+        bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+
+        -- Selects a code action available at the current cursor position
+        bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+
+        -- Show diagnostics in a floating window
+        bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+
+        -- Move to the previous diagnostic
+        bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+
+        -- Move to the next diagnostic
+        bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+      end
+    })
+
+    -- Configure nvim-cmp and luasnip
+    vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+    require('luasnip.loaders.from_vscode').lazy_load()
+
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+
+    local select_opts = {behavior = cmp.SelectBehavior.Select}
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end
+      },
+      sources = {
+        {name = 'path'},
+        {name = 'nvim_lsp', keyword_length = 1},
+        {name = 'buffer', keyword_length = 3},
+        {name = 'luasnip', keyword_length = 2},
+      },
+      window = {
+        documentation = cmp.config.window.bordered()
+      },
+      formatting = {
+        fields = {'menu', 'abbr', 'kind'},
+        format = function(entry, item)
+          local menu_icon = {
+            nvim_lsp = 'λ',
+            luasnip = '⋗',
+            buffer = 'Ω',
+            path = '🖫',
+          }
+
+          item.menu = menu_icon[entry.source.name]
+          return item
+        end,
+      },
+      mapping = {
+        ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+
+        ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-y>'] = cmp.mapping.confirm({select = true}),
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+        ['<C-f>'] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, {'i', 's'}),
+
+        ['<C-b>'] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, {'i', 's'}),
+
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          local col = vim.fn.col('.') - 1
+
+          if cmp.visible() then
+            cmp.select_next_item(select_opts)
+          elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+            fallback()
+          else
+            cmp.complete()
+          end
+        end, {'i', 's'}),
+
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item(select_opts)
+          else
+            fallback()
+          end
+        end, {'i', 's'}),
+      },
+    })
   '';
 in {
 
   home.packages = with pkgs; [
-    # install language server to enable auto-completion on docker-compose files
-    docker-compose-language-service
     nixd
-    #kotlin-language-server
-    #haskell-language-server
+    llvmPackages_15.clang-unwrapped
   ];
 
   programs = {
@@ -132,44 +270,6 @@ in {
       # make sure that vi or vim also points to nvim
       viAlias = true;
       vimAlias = true;
-      
-      # auto-completion
-      coc.enable = true;
-      coc.settings = {
-        "languageserver" = {
-          nix = {
-            command = "nixd";
-            filetypes = ["nix"];
-          };
-          dockerfile = {
-            command = "docker-langserver";
-            filetypes = ["dockerfile"];
-            args = ["--stdio"];
-          };
-          dockercompose = {
-            command = "docker-compose-langserver";
-            args = ["--stdio"];
-            filetypes = ["dockercompose"];
-            rootPatterns = [".git" ".env" "docker-compose.yml" "compose.yml"];
-          };
-#          haskell = {
-#            command = "haskell-language-server-wrapper";
-#            args = [ "--lsp" ];
-#            rootPatterns = [
-#              "*.cabal"
-#              "stack.yaml"
-#              "cabal.project"
-#              "package.yaml"
-#              "hie.yaml"
-#            ];
-#            filetypes = [ "haskell" "lhaskell" ];
-#          };
-#          kotlin = {
-#            command = "kotlin-language-server";
-#            filetypes = ["kotlin"];
-#          };
-        };
-      };
 
       # plugins to use within the editor
       plugins = with pkgs.vimPlugins; [
@@ -181,17 +281,16 @@ in {
         catppuccin-nvim                 # theming
         gitsigns-nvim                   # indicate git changes
         nvim-treesitter.withAllGrammars # syntax highlighting
-        coc-cmake                       # auto-completion for cmake
-        coc-clangd                      # auto-completion for c/c++
-        coc-sh                          # auto-completion for bash 
-        coc-python                      # auto-completion for python
-        coc-json                        # auto-completion json
-        coc-prettier                    # formatting
-        #coc-java                        # auto-completion java
-        #coc-tsserver                    # auto-completion javascript / typescript
+        nvim-lspconfig                  # language server
+        nvim-cmp                        # code completion
+        cmp-buffer                      # nvim-cmp source for buffer words
+        cmp-path                        # nvim-cmp source for filesystem paths
+        cmp-nvim-lsp                    # nvim-cmp source for neovim's built-in language server client
+        friendly-snippets               # Snippets collection for a set of different programming languages
+        luasnip                         # Snip engine
       ];
       
-      extraLuaConfig = utils + keymaps + plugin-config + theme;
+      extraLuaConfig = utils + keymaps + plugin-config + theme + language-servers;
     };
   };
 }
